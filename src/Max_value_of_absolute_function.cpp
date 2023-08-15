05d37c5bc5e8eb6b53b78e48ffbffc94cf79143c
Bhagyashri Bangar <bhagyashrib209@gmail.com>
2023-08-15 10:51:30 -0700

    code cleanup

--- src/Max_value_of_absolute_function.cpp
+++ src/Max_value_of_absolute_function.cpp
@@ -1,15 +1,12 @@
-// Maxmum value of absolute function
+// Maximum value of absolute function
 #include <iostream>
 using namespace std;
-#define MAX 10
 
 // Return maximum value of |arr[i] - arr[j]| + |i - j|
 int findMaxValue(int arr[], int n)
 {
 	int ans = 0;
 
-	// Iterating two for loop, one for
-	// i and another for j.
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			// Evaluating |arr[i] - arr[j]| + |i - j|
@@ -18,10 +15,9 @@ int findMaxValue(int arr[], int n)
 	return ans;
 }
 
-// Driven Program
 int main()
 {
-	int arr[] = { 1, 2, 3, 1 };
+	int arr[] = {2, 5, 6, 7};
 	int n = sizeof(arr) / sizeof(arr[0]);
 
 	cout << findMaxValue(arr, n) << endl;
